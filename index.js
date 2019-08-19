fc00535dcc13c7de51084e8b2c617c62ff5cdc81
jackbolton901 <bltnjck@gmail.com>
2019-08-19 14:17:55 -0500

    afternoon practice

---
+++ index.js
@@ -0,0 +1,21 @@
+let userInput =(prompt("Press 1, 2, 3, or q to quit"));
+function getInput()
+{
+    let userInput =(prompt("Press 1, 2, 3, or q to quit"));
+
+}
+while(userInput !== "q") {
+
+    if (userInput === "1") {
+        alert("Challenge goes here");
+    }
+    else if (userInput === "2"){
+        alert("2");
+}
+        else if(userInput === "3"){
+            alert("Hello World")
+    }
+        getInput()
+}
+
+
